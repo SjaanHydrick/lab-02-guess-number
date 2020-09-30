e00db8760ddbf7812b163591521a6c4d2ede1ea9
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-09-29 17:14:39 -0700

    functional AND pretty what a world

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ const dialogue = document.getElementById('dialogue');
 const mothMan = document.getElementById('moth-man');
 const input = document.getElementById('input');
 const buttonBottom = document.getElementById('protag2');
+const buttonInput = document.getElementById('protag3');
 
 mothMan.style.visibility = 'hidden';
 input.style.visibility = 'hidden';
 buttonBottom.style.visibility = 'hidden';
+buttonInput.style.visibility = 'hidden';
 
 button.addEventListener('click', () => {
     button.style.visibility = 'hidden';
@@ -20,45 +22,46 @@ button.addEventListener('click', () => {
     dialogue.textContent = '\'Sup, human';
     buttonBottom.style.visibility = 'visible';
     buttonBottom.textContent = 'OHMIGOSH';
-    buttonBottom.style.backgroundColor = 'firebrick';
 });
+
+const dialogueOutput = [
+    '',
+    'I have that effect on people',
+    'Moth Monster',
+    'I\'ma eat you now',
+    'Okay, fine, then let\'s play a game',
+    'I\'m thinking of a number, 1 thru 20',
+    'You have three tries to guess right',
+    'What\'s your first guess?'];
+
+const buttonOutputs = [
+    '',
+    'WHAT ARE YOU?',
+    'UH.',
+    'MAYBE DON\'T?',
+    'UH?',
+    'UHHHH?',
+    'OKAY?'];
+
+let dialogueCounter = 0;
+let buttonCounter = 0;
 
 buttonBottom.addEventListener('click', () => {
-    dialogue.textContent = 'I have that effect on people';
-    buttonBottom.textContent = 'WHAT ARE YOU?';
+    dialogueCounter++;
+    buttonCounter++;
+    dialogue.textContent = dialogueOutput[dialogueCounter];
+    buttonBottom.textContent = buttonOutputs[buttonCounter];
 
-    buttonBottom.addEventListener('click', () => {
-        dialogue.textContent = 'Moth Monster';
-        buttonBottom.textContent = 'UH.';
-
-        buttonBottom.addEventListener('click', () => {
-            dialogue.textContent = 'I\'ma eat you now';
-            buttonBottom.textContent = 'MAYBE DON\'T?';
-
-            buttonBottom.addEventListener('click', () => {
-                dialogue.textContent = 'Okay, fine, then let\'s play a game';
-                buttonBottom.textContent = 'UH?';
-                buttonBottom.style.backgroundColor = 'indianred';
-
-                buttonBottom.addEventListener('click', () => {
-                    dialogue.textContent = 'I\'m thinking of a number, 1 thru 20';
-                    buttonBottom.textContent = 'UHHHH?';
-
-                    buttonBottom.addEventListener('click', () => {
-                        dialogue.textContent = 'You have three tries to guess right';
-                        buttonBottom.textContent = 'OKAY?';
-                    });
-                });
-            });
-        });
-    });
-
+    if (buttonCounter === 7) {
+        buttonBottom.style.visibility = 'hidden';
+        buttonInput.style.visibility = 'visible';
+    }
 });
 
 
-// Things to Do:
-// 1) Make mothMan hidden/appear on 1st click
-// 2) Add onClick elements to lead the "story"
+
+
+
 // 3) Create input that appears for number entry
 // 4) ???
 // 5) Profit
